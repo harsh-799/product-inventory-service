245bf9f6654651c2d33ce52ef4b97eb8c9039598
harsh-799 <harshanand7898@gmail.com>
2026-09-03 11:21:08 +0530

    backend: fix-up package name mismatch

--- src/main/java/com/harsh/product/inventory/ProductInventoryServiceApplication.java
+++ src/main/java/com/harsh/product/inventory/ProductInventoryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.harsh.product.inventory.service;
+package com.harsh.product.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
